75749101b41e915f4da66d3f12746a8d1891c727
jum-s <vincent.jumeaux@riseup.net>
2014-01-18 22:04:32 +0100

    initial + Readme

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+GithubConnect::Application.config.secret_key_base = 'b06496197316017348a198bb125c3b7f93683d184a545e67b0a19abecc6958caa360c5b3d244cf08a8f2c2c1d38459daa5c351c5159b6c6d6b000284bc54eb9a'
